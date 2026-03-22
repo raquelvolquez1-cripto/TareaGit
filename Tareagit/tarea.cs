7f4cbe267c3eaad36c8a4326a3805829fb97afe0
raquelvolquez1-cripto <raquelvolquez1@gmail.com>
2026-03-22 12:44:25 -0400

    Update tarea.cs

--- Tareagit/tarea.cs
+++ Tareagit/tarea.cs
@@ -1,2 +1,2 @@
-Console.Writeline("Hola mundo, estoy practicando con github");
-Console.Writeline("Este es mi proggrama base");
+Console.Writeline("Hola mundo, estoy practicando con mi rama2 github");
+Console.Writeline("Este es mi programa de la practica");
